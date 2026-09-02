5d814f6ff8a49a438e3ddfd101316c022d19120c
arnavsx3 <arnavsx3@gmail.com>
2026-09-02 12:18:03 +0530

    add components/About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="font-pixel text-arcade-magenta text-2xl">About</h1>
+    </section>
+  );
+}
